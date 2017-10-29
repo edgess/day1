1f3f556e0bb23ddaa93154a6ebe53ef811745b60
edge <edge2@126.com>
2017-10-29 23:56:16 +0800

    bit

--- src/test/java/com/mav/day1/AppTest.java
+++ src/test/java/com/mav/day1/AppTest.java
@@ -1,5 +1,7 @@
 package com.mav.day1;
 
+import org.apache.ibatis.session.SqlSession;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,8 +9,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase
 {
     /**
      * Create the test case
@@ -33,6 +34,16 @@ public class AppTest
      */
     public void testApp()
     {
-        assertTrue( true );
+    	System.out.println("123");
+    	
+    	SqlSession sqlSession = MyBatisUtil.getSqlSession();
+
+        String statement = "com.mav.day1.classMapper.getClass";//映射sql的标识字符串
+        //执行查询操作，将查询结果自动封装成Order对象返回
+        Classes classes = sqlSession.selectOne(statement,1);//查询orders表中id为1的记录
+        //使用SqlSession执行完SQL之后需要关闭SqlSession
+        sqlSession.close();
+        System.out.println(classes);//打印结果：null，也就是没有查询出相应的记录
+
     }
 }
